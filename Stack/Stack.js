33558d21e275a8491551133e5245817294ecbc3c
Douglas de Lima <douglasandreilima@gmail.com>
2021-01-13 20:12:34 -0300

    Add methods base converter

--- Stack/Stack.js
+++ Stack/Stack.js
@@ -33,13 +33,65 @@ class Stack {
     };
 }
 
-let stack = new Stack();
-console.log(stack.isEmpty());
+// let stack = new Stack();
+// console.log(stack.isEmpty());
 
-stack.push(5);
-stack.push(8);
+// stack.push(5);
+// stack.push(8);
 
 
-stack.print();
-console.log(stack.pop());
-stack.print();
+// stack.print();
+// console.log(stack.pop());
+// stack.print();
+
+
+// DECIMAL PARA BINÁRIO
+
+function divideBy2(decNumber){
+
+    var remStack = new Stack();
+    var rem;
+    var binaryString = '';
+
+    while(decNumber > 0) {
+
+        rem = Math.floor(decNumber % 2);
+        remStack.push(rem);
+        decNumber = Math.floor(decNumber / 2);
+    }
+
+    while(!remStack.isEmpty()){
+        binaryString+= remStack.pop().toString();
+    }
+
+    return binaryString;
+}
+
+// console.log('teste');
+// console.log(divideBy2(233));
+
+// CONVERSOR DE BASE
+
+function baseConverter(decNumber, base) {
+
+    var remStack = new Stack();
+    var rem;
+    var baseString = '';
+    var digits = '0123456789ABCDEF';
+
+    while(decNumber > 0) {
+
+        rem = Math.floor(decNumber % 2);
+        remStack.push(rem);
+        decNumber = Math.floor(decNumber / base);
+    }
+
+    while(!remStack.isEmpty()){
+        baseString+= digits[remStack.pop()];
+    }
+
+    return baseString;
+
+}
+
+// console.log(baseConverter(10,2));
